5c86eceafce809b0540a2454fbad5003989043b0
Joe Watts <squarepeoplesolutions@gmail.com>
2023-09-08 06:53:05 +0100

    update meta

--- common/DocumentHead.tsx
+++ common/DocumentHead.tsx
@@ -15,6 +15,18 @@ const DocumentHead = () => {
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1"
         />
+        <meta name="title" content="Car Pool Calculator" />
+        <meta
+          name="description"
+          content="Car pool calculator works out complex travel arrangements, estimates fuel costs, and can share the result with business trip colleagues or friends"
+        />
+        <meta
+          name="keywords"
+          content="Car Pool Calculator, Car Pool, Calculator, Group Trip, Business Trip, Road Trip, Fuel Estimate"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalStyle />
